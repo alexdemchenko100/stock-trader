2613ba0b132a02a852bf1ac63fb82d48078bf00f
alexdemchenko <alexdemchenko100@gmail.com>
2020-06-24 00:22:34 +0300

    adds Stock and Stocks components

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -1,3 +1,23 @@
 <template>
-  <h1>The Stocks - Stock Component</h1>
+  <div class="col-sm-6 col-md-4">
+    <div class="card border-success mb-3" >
+      <div class="card-header ">
+        NAME
+        <smal>(Price: PRICE)</smal>
+      </div>
+      <div class="card-body text-success">
+        <div class="float-left">
+          <input
+            class="form-control"
+            placeholder="Quantity"
+            type="number">
+        </div>
+        <div class="float-right">
+          <button class="btn btn-success">
+            Buy
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
 </template>
--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,27 @@
 <template>
-  <h1>The Stocks Component</h1>
+  <div class="container">
+    <div class="row">
+        <app-stock v-for="stock in stocks"></app-stock>
+    </div>
+  </div>
 </template>
+
+<script>
+  import Stock from './Stock.vue'
+
+  export default {
+    data () {
+      return {
+        stocks: [
+          { id: 1, name: 'BMW', price: 115 },
+          { id: 2, name: 'Google', price: 180 },
+          { id: 3, name: 'Apple', price: 235 },
+          { id: 4, name: 'Twitter', price: 13 }
+        ]
+      }
+    },
+    components: {
+      appStock: Stock
+    }
+  }
+</script>
