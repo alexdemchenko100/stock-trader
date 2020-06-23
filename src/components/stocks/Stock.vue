a717c89fce0d888401489a64424cf250103ee920
alexdemchenko <alexdemchenko100@gmail.com>
2020-06-24 00:37:33 +0300

    adds props to Stock component

--- src/components/stocks/Stock.vue
+++ src/components/stocks/Stock.vue
@@ -2,22 +2,35 @@
   <div class="col-sm-6 col-md-4">
     <div class="card border-success mb-3" >
       <div class="card-header ">
-        NAME
-        <smal>(Price: PRICE)</smal>
+        {{ stock.name }}
+        <smal>(Price: {{ stock.price }})</smal>
       </div>
       <div class="card-body text-success">
         <div class="float-left">
           <input
             class="form-control"
             placeholder="Quantity"
-            type="number">
+            type="number"
+            v-model="quantity"
+          >
         </div>
         <div class="float-right">
-          <button class="btn btn-success">
-            Buy
-          </button>
+          <button
+            class="btn btn-success"
+          >Buy</button>
         </div>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+  export default {
+    props: ['stock'],
+    data () {
+      return {
+        quantity: 0
+      }
+    }
+  }
+</script>
